944c5978b1fc47403c172fac336aeb8a5b0dd68f
rkoh-rq <49420412+rkoh-rq@users.noreply.github.com>
2020-10-26 23:11:42 +0800

    Added LoginMgr, FileReader, CourseMgr

---
+++ src/FileReader.java
@@ -0,0 +1,18 @@
+public class FileReader{
+    private String filePath;
+    public FileReader(){
+
+    }
+    public String getPassword(String userId){
+        return "";
+    }
+    public String getInfo(String userId){
+        return "";
+    }
+    public String getCourseInfo(String courseCode){
+        return "";
+    }
+    public boolean changeUserDetails(String[] details){
+        return true;
+    }
+}
---
+++ src/StaffSystem.java
@@ -0,0 +1,21 @@
+public class StaffSystem {
+    FileReader fileReader;
+    String studentDetailsFilePath;
+    String courseDetailsFilePath;
+
+    public StaffSystem(){
+
+    }
+    public boolean updateAccessPeriod(String matricNo){
+        return true;
+    }
+    public void addStudent(Object[] details){
+
+    }
+    public void updateCourse(String courseCode, Object[] details){
+
+    }
+    public void addCourse(Course course){
+        
+    }
+}
