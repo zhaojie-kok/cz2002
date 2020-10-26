9378cf2b891c1d7bc9bd15247eadb9e03a17a08b
jezebellee <70324766+jezebellee@users.noreply.github.com>
2020-10-26 21:50:06 +0800

    Update Index.java

--- src/Index.java
+++ src/Index.java
@@ -1,1 +1,81 @@
+import java.util.List;
+
+public class Index {
+
+	private int indexNo;
+	private int slotsAvailable;
+	private int slotsTotal;
+	private List<Student> registeredStudents; 
+	private List<LessonDetails> timeTable;
+	
+	public int getIndexNo()
+	{
+		return indexNo;
+	}
+	
+	public void setIndexNo(int indexNo)
+	{
+		this.indexNo = indexNo;
+	}
+	
+	public int getSlotsAvailable()
+	{
+		return slotsAvailable;
+	}
+	
+	public void setSlotsAvailable(int slotsAvailable)
+	{
+		this.slotsAvailable = slotsAvailable;
+	}
+	
+	public int getSlotsTotal()
+	{
+		return slotsTotal;
+	}
+	
+	public void setSlotsTotal(int slotsTotal)
+	{
+		this.slotsTotal = slotsTotal;
+	}
+	
+	public List<Student> getRegisteredStudents()
+	{
+		return registeredStudents;
+	}
+	
+	public void setRegisteredStudents(List<Student> registeredStudents)
+	{
+		this.registeredStudents = registeredStudents;
+	}
+	
+	public List<LessonDetails> getTimeTable()
+	{
+		return timeTable;
+	}
+	
+	public void setTimeTable(List<LessonDetails> timeTable)
+	{
+		this.timeTable = timeTable;
+	}
+	
+	public void addSlotsAvailable()
+	{
+		this.slotsAvailable++;
+	}
+	
+	public void minusSlotsAvailable()
+	{
+		this.slotsAvailable--;
+	}
+	
+	public void addSlotsTotal()
+	{
+		this.slotsTotal++;
+	}
+	
+	public void minusSlotsTotal()
+	{
+		this.slotsTotal--;
+	}
+}
 
