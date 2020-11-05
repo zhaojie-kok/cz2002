f1a9f2ce21eb2cba43257cad98c0eb8fab0f741f
subiksharaman1 <60492920+subiksharaman1@users.noreply.github.com>
2020-11-05 16:29:48 +0800

    Updated StaffSystem

--- src/StaffSystem.java
+++ src/StaffSystem.java
@@ -8,7 +8,17 @@ public class StaffSystem {
     String courseDetailsFilePath;
 
 
-    public boolean updateAccessPeriod(String matricNo){
+
+    public boolean updateAccessPeriod(Student student){
+        StudentManager smgr = new StudentManager(); // check constructor
+        Date accessPeriod[] = student.getAccessPeriod();
+
+        // do we need to check accessPeriod? what do we update it to?
+        if (accessPeriod[] = newAccessPeriod){
+            // same/updated alr
+            return false;
+        }
+        student.changeAccessPeriod(newAccessPeriod);
         return true;
     }
     public void addStudent(Object[] details){
@@ -22,8 +32,40 @@ public class StaffSystem {
     public void updateCourse(String courseCode, Object[] details){
         
     }
-    public void addCourse(Course course){
-        
+    public void addCourse(){
+        String courseCode = course.getCourseCode();
+        String school = course.getSchool();
+        int acadU = course.getAcadU();
+        Calendar examDate = course.getExamDate();
+
+        CourseMgr cmgr = new CourseMgr(); // check constructor
+        createCourse(courseCode, school, acadU, examDate);
+        createIndex(courseCode, indexNo)
+
+    }
+
+
+    public int checkAvailableVacancies(Course course){
+        CourseMgr cmgr = new CourseMgr(); // check constructor
+        cmgr.checkAvailableVacancies(course);
+        // anything else needs to be done?
+    }
+
+    public void printStudentsbyIndex(){
+        HashMap<String, Student> students = FileReader.loadStudents();
+        // TODO
+    }
+
+    public void printStudentsbyCourse(String courseCode){
+        HashMap<String, Student> students = FileReader.loadStudents();
+        // in the above hashmap string is matricNo
+        for (Student s : students.values()){
+            HashMap<String, String> courses = s.getCourses();
+            for (String cc : courses.keySet())
+                if (courseCode == cc){
+                    System.out.printf(s.name + s.gender + s.nationality + "\n");
+                }
+        }
     }
     
 }
