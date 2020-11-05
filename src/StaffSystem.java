8122802c67357fd74a0443691d24d775b6b24377
subiksharaman1 <60492920+subiksharaman1@users.noreply.github.com>
2020-11-05 15:09:38 +0800

    Updated StaffSystem

--- src/StaffSystem.java
+++ src/StaffSystem.java
@@ -1,21 +1,29 @@
+import java.util.HashMap;
+
 public class StaffSystem {
+    private static HashMap<String, Staff> staff = FileReader.loadStaff();
+
     FileReader fileReader;
     String studentDetailsFilePath;
     String courseDetailsFilePath;
 
-    public StaffSystem(){
 
-    }
     public boolean updateAccessPeriod(String matricNo){
         return true;
     }
     public void addStudent(Object[] details){
-
+        // iterate through Object[] 
+        // take each attribute
+        // instantiate Student object by putting in
+        // each of the attributes into args of constructor
+        
+        Student student = new Student()
     }
     public void updateCourse(String courseCode, Object[] details){
-
+        
     }
     public void addCourse(Course course){
         
     }
+    
 }
