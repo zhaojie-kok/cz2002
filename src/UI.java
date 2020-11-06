35255c7b0aed636da64b6004832efef28d4cba5b
JaclynToh <C190137@e.ntu.edu.sg>
2020-11-06 14:48:29 +0800

    Added methods

---
+++ src/UI.java
@@ -0,0 +1,19 @@
+public class UI{
+    private String userID;
+    private String password;
+
+    public void showLoginUI()
+    {
+
+    }
+
+    public void showStaffUI()
+    {
+
+    }
+
+    public void showStudentUI()
+    {
+        
+    }
+}
