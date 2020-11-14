efe5d7fe9a5a0cdfe216d8602585f93c05b0a720
chrispyscream <kok.zhaojie@gmail.com>
2020-11-15 01:02:45 +0800

    update gitignore

--- src/UI.java
+++ src/UI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public interface UI {
     // a generalised UI interface so that other kinds of UI can be created from this
 
@@ -15,24 +14,3 @@ public interface UI {
     // all kinds of UI should be able to show output to user
     public void displayOutput(Object toDisplay);
 }
-=======
-public class UI{
-    private String userID;
-    private String password;
-
-    public void showLoginUI()
-    {
-
-    }
-
-    public void showStaffUI()
-    {
-
-    }
-
-    public void showStudentUI()
-    {
-        
-    }
-}
->>>>>>> af71168b9cf270b4a986153ec33056f4387832a2
