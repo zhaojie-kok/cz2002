4428a0a67c95e2f067b0301ec3592190e20fd145
rkoh-rq <49420412+rkoh-rq@users.noreply.github.com>
2020-11-22 22:04:21 +0800

    Some changes to messages
    Can ignore changes to testapp

--- src/exceptions/KeyNotFoundException.java
+++ src/exceptions/KeyNotFoundException.java
@@ -6,8 +6,8 @@ public class KeyNotFoundException extends Exception {
      */
     private static final long serialVersionUID = -7820625580190434595L;
     String message;
-    public KeyNotFoundException(String message){
-        this.message = message;
+    public KeyNotFoundException(String key){
+        this.message = key + " not found";
     }
 
     public String getMessage(){
