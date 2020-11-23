e89fdc12c15d72643430f1176be6be9d1237a703
rkoh-rq <49420412+rkoh-rq@users.noreply.github.com>
2020-11-23 20:49:52 +0800

    getLessInfo

--- src/entities/Printable.java
+++ src/entities/Printable.java
@@ -4,6 +4,11 @@ package entities;
  * Entity interface for entities that have information meant to be shown to the user
  */
 public interface Printable {
+    /** 
+     * Method to retrieve an even shorter version of getInfo, for use in long lists 
+     * @return A string containing the information
+     */
+    public String getLessInfo();
     /**
      * Method to retrieve basic or most commonly used information about the entity
      * @return A string containing the information
@@ -12,7 +17,7 @@ public interface Printable {
 
     /**
      * Method to retrieve detailed or rarely used information about the entity
-     * @return A string contraining the information
+     * @return A string containing the information
      */
     public String getMoreInfo();
 }
